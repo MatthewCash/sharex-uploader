6160ef939194ab7477b78a5e00c66412124869e8
Matthew_Cash <matthew@matthew-cash.com>
2019-09-16 21:36:26 -0700

    Set MIME Type for raw format

--- api/image.js
+++ api/image.js
@@ -1,54 +1,76 @@
 require("dotenv").config();
 const Image = require("./models/Image");
 
-//---------
-//-Methods-
-//---------
+// ---------
+// -Methods-
+// ---------
 
 async function saveImage(req, res, next) {
-  let auth = req.header("Authentication");
-  if (auth) {
-    if (auth != "ca03849frhyw98ptrphajat8op9sd") {
-      return res.status(401).json({ message: "Authentication Failed!" });
+    const auth = req.header("Authentication");
+    if (auth) {
+        if (auth != "ca03849frhyw98ptrphajat8op9sd") {
+            return res.status(401).json({ message: "Authentication Failed!" });
+        }
+    } else {
+        return res
+            .status(401)
+            .json({ message: "Please specify authentication!" });
     }
-  } else {
-    return res.status(401).json({ message: "Please specify authentication!" });
-  }
 
-  let image = new Image({
-    data: req.file.buffer
-  });
-
-  image
-    .save()
-    .then(image => {
-      res.status(200).send(process.env.RETURN_URL + image._id);
-    })
-    .catch(error => {
-      console.error("Error saving image to database: " + error);
-      res.status(400).json({ message: "Error saving!" });
+    const image = new Image({
+        data: req.file.buffer
     });
+
+    image
+        .save()
+        .then(image => {
+            res.status(200).send(process.env.RETURN_URL + image._id);
+        })
+        .catch(error => {
+            console.error("Error saving image to database: " + error);
+            res.status(400).json({ message: "Error saving!" });
+        });
 }
 
 async function getImage(req, res, next) {
-  Image.findById(req.params.image, (error, image) => {
-    if (error || !image)
-      return res.status(404).json({ message: "Cannot find Image!" });
-    res.status(200).send({ image: image.data, time: image.createdAt });
-  });
+    Image.findById(req.params.image, (error, image) => {
+        if (error || !image)
+            return res.status(404).json({ message: "Cannot find Image!" });
+        res.status(200).send({ image: image.data, time: image.createdAt });
+    });
 }
 
 async function getImageRaw(req, res, next) {
-  Image.findById(req.params.image, (error, image) => {
-    if (error || !image)
-      return res.status(404).json({ message: "Cannot find Image!" });
-    res.status(200).send(image.data);
-  });
+    Image.findById(req.params.image, (error, image) => {
+        if (error || !image)
+            return res.status(404).json({ message: "Cannot find Image!" });
+        let contentType;
+        switch (
+            Buffer.from(image.data)
+                .toString("base64")
+                .charAt(0)
+        ) {
+            case "/":
+                contentType = "image/jpeg";
+                break;
+            case "i":
+                contentType = "image/png";
+                break;
+            case "R":
+                contentType = "image/gif";
+                break;
+            case "U":
+                contentType = "image/webp";
+                break;
+        }
+        res.status(200)
+            .set("Content-Type", contentType)
+            .send(image.data);
+    });
 }
-
-//--------
-//-Routes-
-//--------
+// --------
+// -Routes-
+// --------
 
 module.exports.saveImage = saveImage;
 module.exports.getImage = getImage;
